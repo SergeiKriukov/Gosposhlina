84b12775509ea86246068d8508099812481411ee
Sergey Kryukov <41209320+SergeiKriukov@users.noreply.github.com>
2024-08-31 23:55:11 +0300

    update

--- Sources/Gosposhlina/Calculations.swift
+++ Sources/Gosposhlina/Calculations.swift
@@ -278,9 +278,9 @@ public class Calculations {
                     // Суд общей юрисдикции, первая инстанция
                     if amount <= 20000 {
                         calculatedAmount = max(400, amount * 0.04)
-                        textResultSOU = String(format: "4 %% от %.2f руб. = ", amount, calculatedAmount) + String(amount * 4 / 100) + " руб."
+                        textLabel = String(format: "4 %% от %.2f руб. = ", amount, calculatedAmount) + String(amount * 4 / 100) + " руб."
                         if calculatedAmount == 400 {
-                            textResultSOU += " Берётся 400 руб."
+                            textLabel += " Берётся 400 руб."
                         }
                     } else if amount <= 100000 {
                         calculatedAmount = 800 + (amount - 20000) * 0.03
@@ -369,18 +369,18 @@ public class Calculations {
                         }
                     } else if amount <= 200000 {
                         calculatedAmount = round(4000 + (amount - 100000) * 0.03)
-                        textLabel += String(format: "4000 руб. + 3 %% от (%.2f руб. - 100000 руб.) = %.2f руб. Берётся %.2f руб., округляется до целого числа.", amount, calculatedAmount, calculatedAmount)
+                        textLabel = String(format: "4000 руб. + 3 %% от (%.2f руб. - 100000 руб.) = %.2f руб. Берётся %.2f руб., округляется до целого числа.", amount, calculatedAmount, calculatedAmount)
                     } else if amount <= 1000000 {
                         calculatedAmount = 7000 + (amount - 200000) * 0.02
-                        textLabel += String(format: "7000 руб. + 2 %% от (%.2f руб. - 200000 руб.) = %.2f руб. Берётся %.2f руб., округляется до целого числа.", amount, calculatedAmount, calculatedAmount)
+                        textLabel = String(format: "7000 руб. + 2 %% от (%.2f руб. - 200000 руб.) = %.2f руб. Берётся %.2f руб., округляется до целого числа.", amount, calculatedAmount, calculatedAmount)
                         
                     } else if amount <= 2000000 {
                         calculatedAmount = 23000 + (amount - 1000000) * 0.01
-                        textLabel += String(format: "23000 руб. + 1 %% от (%.2f руб. - 1000000 руб.) = %.2f руб. Берётся %.2f руб., округляется до целого числа.", amount, calculatedAmount, calculatedAmount)
+                        textLabel = String(format: "23000 руб. + 1 %% от (%.2f руб. - 1000000 руб.) = %.2f руб. Берётся %.2f руб., округляется до целого числа.", amount, calculatedAmount, calculatedAmount)
                         
                     } else {
                         calculatedAmount = min(33000 + (amount - 2000000) * 0.005, 200000)
-                        textLabel += String(format: "33000 руб. + 0.5 %% от (%.2f руб. - 2000000 руб.) = " + String(33000 + (amount - 2000000) * 0.005) + " руб. Берётся %.2f руб.", amount, calculatedAmount, calculatedAmount)
+                        textLabel = String(format: "33000 руб. + 0.5 %% от (%.2f руб. - 2000000 руб.) = " + String(33000 + (amount - 2000000) * 0.005) + " руб. Берётся %.2f руб.", amount, calculatedAmount, calculatedAmount)
                     }
                     
                     // Если приказ
