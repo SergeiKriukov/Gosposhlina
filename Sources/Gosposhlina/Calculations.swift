214d15069f034c74095b5aa78bcb78d38df1d277
Sergey Kryukov <41209320+SergeiKriukov@users.noreply.github.com>
2024-08-31 23:47:28 +0300

    update

--- Sources/Gosposhlina/Calculations.swift
+++ Sources/Gosposhlina/Calculations.swift
@@ -362,25 +362,25 @@ public class Calculations {
                     // расчеты госпошлины по первой инстанции в АС
                     
                     if amount <= 100000 {
-                        calculatedAmount2 = max(2000, amount * 0.04)
+                        calculatedAmount = max(2000, amount * 0.04)
                         textResultAS = String(format: "4 %% от %.2f руб. = ", amount, calculatedAmount) + String(amount * 4 / 100) + " руб."
-                        if calculatedAmount2 == 2000 {
+                        if calculatedAmount == 2000 {
                             textResultAS += " Берётся 2000 руб."
                         }
                     } else if amount <= 200000 {
-                        calculatedAmount2 = round(4000 + (amount - 100000) * 0.03)
-                        textResultAS += String(format: "4000 руб. + 3 %% от (%.2f руб. - 100000 руб.) = %.2f руб. Берётся %.2f руб., округляется до целого числа.", amount, calculatedAmount2, calculatedAmount2)
+                        calculatedAmount = round(4000 + (amount - 100000) * 0.03)
+                        textResultAS += String(format: "4000 руб. + 3 %% от (%.2f руб. - 100000 руб.) = %.2f руб. Берётся %.2f руб., округляется до целого числа.", amount, calculatedAmount, calculatedAmount)
                     } else if amount <= 1000000 {
-                        calculatedAmount2 = 7000 + (amount - 200000) * 0.02
-                        textResultAS += String(format: "7000 руб. + 2 %% от (%.2f руб. - 200000 руб.) = %.2f руб. Берётся %.2f руб., округляется до целого числа.", amount, calculatedAmount2, calculatedAmount2)
+                        calculatedAmount = 7000 + (amount - 200000) * 0.02
+                        textResultAS += String(format: "7000 руб. + 2 %% от (%.2f руб. - 200000 руб.) = %.2f руб. Берётся %.2f руб., округляется до целого числа.", amount, calculatedAmount, calculatedAmount)
                         
                     } else if amount <= 2000000 {
-                        calculatedAmount2 = 23000 + (amount - 1000000) * 0.01
-                        textResultAS += String(format: "23000 руб. + 1 %% от (%.2f руб. - 1000000 руб.) = %.2f руб. Берётся %.2f руб., округляется до целого числа.", amount, calculatedAmount2, calculatedAmount2)
+                        calculatedAmount = 23000 + (amount - 1000000) * 0.01
+                        textResultAS += String(format: "23000 руб. + 1 %% от (%.2f руб. - 1000000 руб.) = %.2f руб. Берётся %.2f руб., округляется до целого числа.", amount, calculatedAmount, calculatedAmount)
                         
                     } else {
-                        calculatedAmount2 = min(33000 + (amount - 2000000) * 0.005, 200000)
-                        textResultAS += String(format: "33000 руб. + 0.5 %% от (%.2f руб. - 2000000 руб.) = " + String(33000 + (amount - 2000000) * 0.005) + " руб. Берётся %.2f руб.", amount, calculatedAmount2, calculatedAmount2)
+                        calculatedAmount = min(33000 + (amount - 2000000) * 0.005, 200000)
+                        textResultAS += String(format: "33000 руб. + 0.5 %% от (%.2f руб. - 2000000 руб.) = " + String(33000 + (amount - 2000000) * 0.005) + " руб. Берётся %.2f руб.", amount, calculatedAmount, calculatedAmount)
                     }
                     
                     // Если приказ
