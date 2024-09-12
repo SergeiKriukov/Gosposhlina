5b9a7c9a1e71e112b958a722cac68093a1b15ed2
Sergey Kryukov <41209320+SergeiKriukov@users.noreply.github.com>
2024-09-12 11:32:06 +0300

    update

--- Sources/Gosposhlina/Calculations.swift
+++ Sources/Gosposhlina/Calculations.swift
@@ -131,7 +131,7 @@ public class Calculations {
                             }
                             
 
-                    textLabel = "---"
+                    textLabel = ("\(calculatedAmount)")
                    
                 }
                 
